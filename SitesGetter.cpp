f3845465ed48f1c0d353f9d97801bdc5d8a31a08
Kvant85 <kvant85@mail.ru>
2024-04-08 16:07:55 +0300

    Some fixes

--- SitesGetter.cpp
+++ SitesGetter.cpp
@@ -12,12 +12,13 @@ const char* DB_NAME = "sites.db";
 //Тайм-аут запроса (мс)
 const cpr::Timeout tmOut = 5000;
 //Глубина поиска (число букв)
-const int deep = 2;
+const int deep = 3;
 //Требуемое число потоков
-const int numOfThreads = 25;
+const int numOfThreads = 500;
 //=================================
 
 mutex mtx;
+int currentURL = 0;	//Указатель текущего URL
 
 //Проверка на подключение к базе
 #define SQL_CHECK_CONNECTION if (result != SQLITE_OK) { cout << "Error: " << sqlite3_errmsg(db) << endl; sqlite3_close(db); return; }
@@ -49,56 +50,79 @@ void createTable()
 
 void get(vector<string>* _URL, int _numberOfThread)
 {
-	char* err_msg = 0;	//SQL-ошибка
-	for (int i = _numberOfThread; i < _URL->size(); i+= numOfThreads)
+	int usingURL;
+	while (true)
 	{
-		//Получение данных по сайту
-		cpr::Url URL = _URL->at(i);
-		string data = cpr::Get(URL, tmOut).text;
-		if (data != "")
+		mtx.lock();
+		usingURL = currentURL;
+		currentURL++;
+		mtx.unlock();
+		if (usingURL < _URL->size())
 		{
-			mtx.lock();
-			cout << URL << ": getting data." << endl;
-			//Вытаскиваем title страницы
-			string find_begin = "<title>", find_end = "</title>";
-			size_t search_begin = data.find(find_begin), search_end = data.find(find_end);
-			string title = data.substr(search_begin + find_begin.size(), search_end - search_begin - find_begin.size());
-
-			//Добавление сайта в базу
-			sqlite3* db;
-			sqlite3_stmt* res;
-			int result = sqlite3_open(DB_NAME, &db);
-			SQL_CHECK_CONNECTION	//Проверка на подключение к базе
-
-			string sql_query = "UPDATE sites SET title = ?, is_empty = 'NOT EMPTY' WHERE URL = '" + _URL->at(i) + "';";
-			result = sqlite3_prepare_v2(db, sql_query.c_str(), -1, &res, 0);
-			if (result == SQLITE_OK)
+			char* err_msg = 0;	//SQL-ошибка
+			//Получение текущего URLа
+			//Получение данных по сайту
+			cpr::Url URL = _URL->at(usingURL);
+			string data;
+			try
 			{
-				sqlite3_bind_text(res, 1, title.c_str(), -1, SQLITE_STATIC);
-				sqlite3_step(res);
+				data = cpr::Get(URL, tmOut).text;
 			}
-			else SQL_ERROR_COUT
+			catch (exception e)
+			{
+				//cout << "Exception! " << e.what() << endl;
+				data = "None";
+			}
+			if (data != "")
+			{
+				mtx.lock();
+				cout << usingURL << ": " << URL << ": getting data." << endl;
+				//Вытаскиваем title страницы
+				string find_begin = "<title>", find_end = "</title>";
+				size_t search_begin = data.find(find_begin), search_end = data.find(find_end);
+				string title;
+				if (search_begin != string::npos && search_end != string::npos)
+					title = data.substr(search_begin + find_begin.size(), search_end - search_begin - find_begin.size());
+				else
+					title = "TITLE NOT FOUND!";
 
-			sqlite3_finalize(res);
-			sqlite3_close(db);
-			mtx.unlock();
-		}
-		else
-		{
-			mtx.lock();
-			cout << URL << ": empty or TimeOut." << endl;
-			string sql_query = "UPDATE sites SET is_empty = 'EMPTY' WHERE URL = '" + _URL->at(i) + "';";
-			sqlite3* db;
-			int result = sqlite3_open(DB_NAME, &db);
-			SQL_CHECK_CONNECTION_MTX	//Проверка на подключение к базе
-			result = sqlite3_exec(db, sql_query.c_str(), 0, 0, &err_msg);
-			if (result != SQLITE_OK) SQL_ERROR_COUT
+				//Добавление данных сайта в базу
+				sqlite3* db;
+				sqlite3_stmt* res;
+				int result = sqlite3_open(DB_NAME, &db);
+				SQL_CHECK_CONNECTION	//Проверка на подключение к базе
 
-			sqlite3_close(db);
-			mtx.unlock();
+				string sql_query = "UPDATE sites SET title = ?, is_empty = 'NOT EMPTY' WHERE URL = '" + _URL->at(usingURL) + "';";
+				result = sqlite3_prepare_v2(db, sql_query.c_str(), -1, &res, 0);
+				if (result == SQLITE_OK)
+				{
+					sqlite3_bind_text(res, 1, title.c_str(), -1, SQLITE_STATIC);
+					sqlite3_step(res);
+				}
+				else SQL_ERROR_COUT
+
+				sqlite3_finalize(res);
+				sqlite3_close(db);
+				mtx.unlock();
+			}
+			else
+			{
+				mtx.lock();
+				cout << usingURL << ": " << URL << ": empty or TimeOut." << endl;
+				string sql_query = "UPDATE sites SET is_empty = 'EMPTY' WHERE URL = '" + _URL->at(usingURL) + "';";
+				sqlite3* db;
+				int result = sqlite3_open(DB_NAME, &db);
+				SQL_CHECK_CONNECTION_MTX	//Проверка на подключение к базе
+					result = sqlite3_exec(db, sql_query.c_str(), 0, 0, &err_msg);
+				if (result != SQLITE_OK) SQL_ERROR_COUT
+
+				sqlite3_close(db);
+				mtx.unlock();
+			}
+			sqlite3_free(err_msg);
 		}
+		else break;
 	}
-	sqlite3_free(err_msg);
 }
 
 void generate_URL(char first, char last, string _domain, vector<string>* _URL)
@@ -130,11 +154,13 @@ void generate_URL(char first, char last, string _domain, vector<string>* _URL)
 			++ * it;
 		}
 	}
-	cout << "Generation complete. Writing URLs to DB..." << endl;
+	cout << "Generated " << _URL->size() << " URLs." << endl;
+	cout << "Generation complete." << endl;
 }
 
 void writeURLToDB(vector<string>* _URL)
 {
+	cout << "Writing URLs to DB..." << endl;
 	//Запись в базу
 	sqlite3* db;
 	int result = sqlite3_open(DB_NAME, &db);
@@ -143,7 +169,7 @@ void writeURLToDB(vector<string>* _URL)
 	for (int i = 0; i < _URL->size(); i++)
 	{
 		string query = "INSERT OR IGNORE INTO sites ('URL') VALUES ('" + _URL->at(i) + "');";
-		result = sqlite3_exec(db, query.c_str(), 0, 0, &err_msg);
+		result = sqlite3_exec(db, query.c_str(), 0, 0, 0);
 		if (result != SQLITE_OK) SQL_ERROR_COUT
 	}
 	sqlite3_close(db);
@@ -161,6 +187,8 @@ int main()
 	cout << "Type domain to search in (ex: https://abc.XXX - type \"XXX\": ";
 	cin >> domain;
 
+	//Время начала процесса
+	chrono::time_point<chrono::system_clock> startTime = chrono::system_clock::now();
 	//Генерация URL запросов с сайтами от буквы первого аргумента до буквы второго аргумента
 	generate_URL('a', 'z', domain, &URL);
 	//Запись URLов в базу
@@ -172,4 +200,10 @@ int main()
 		t[i] = thread(static_cast<void(*)(vector<string>*, int)> (get), &URL, i);
 	for (int i = 0; i < numOfThreads; i++)
 		t[i].join();
+
+	//Время окончания процесса
+	chrono::time_point<chrono::system_clock> finishTime = chrono::system_clock::now();
+	//Вывод времени процесса
+	double elapsedTime = chrono::duration_cast<chrono::milliseconds>(finishTime - startTime).count();
+	cout << endl << "Time process: " << elapsedTime / 1000 << " seconds." << endl << endl;
 }
